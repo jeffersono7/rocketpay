e8bbe7a5a3e81c3be1ff46b93573bdf08b247a0d
Jefferson Farias <jefferson.farias7@gmail.com>
2021-02-25 09:28:41 -0300

    [core] fazendo preload da conta do usuario

--- lib/rocketpay/users/create.ex
+++ lib/rocketpay/users/create.ex
@@ -6,11 +6,12 @@ defmodule Rocketpay.Users.Create do
   def call(params) do
     Multi.new()
     |> Multi.insert(:create_user, User.changeset(params))
-    |> Multi.run(:create_account, fn repo, %{create_user: user} -> insert_account(repo, user) end)
+    |> Multi.run(:create_account, &insert_account/2)
+    |> Multi.run(:preload_data, &preload_data/2)
     |> run_transaction()
   end
 
-  defp insert_account(repo, user) do
+  defp insert_account(repo, %{create_user: user}) do
     user.id
     |> account_changeset()
     |> repo.insert()
@@ -25,7 +26,11 @@ defmodule Rocketpay.Users.Create do
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
       {:error, _operation, reason, _changes} -> {:error, reason}
-      {:ok, %{create_user: account}} -> IO.inspect(account)
+      {:ok, %{preload_data: user}} -> {:ok, user}
     end
+  end
+
+  defp preload_data(repo, %{create_user: user}) do
+    {:ok, repo.preload(user, :account)}
   end
 end
